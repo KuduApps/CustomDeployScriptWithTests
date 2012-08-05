aa3944b9978b1e069afb806c9fb6ffbab51240ec
Raquel Almeida <raquel_soares@msn.com>
2012-08-05 09:22:27 -0700

    Initial commit

---
+++ FunctionalTests/FunctionalTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace FunctionalTests
+{
+    public class FunctionalTests
+    {
+        [Fact]
+        public void BogusTest()
+        {
+            Assert.True(true);
+            // Assert.False(true);
+        }
+    }
+}
